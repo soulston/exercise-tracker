36b0b148f01472881d9afb9bc5282aa1384efc8d
soulston <simonpoulston@gmail.com>
2020-08-13 22:51:45 +0100

    udpate

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Navbar from "./components/navbar.component"
 import ExercisesList from "./components/exercises-list.component";
-// import EditExercise from "./components/edit-exercise.component";
-// import CreateExercise from "./components/create-exercise.component";
-// import CreateUser from "./components/create-user.component";
+import EditExercise from "./components/edit-exercise.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
@@ -15,9 +15,9 @@ function App() {
       <Navbar />
       <br/>
       <Route path="/" exact component={ExercisesList} />
-      {/* <Route path="/edit/:id" component={EditExercise} /> */}
-      {/* <Route path="/create" component={CreateExercise} /> */}
-      {/* <Route path="/user" component={CreateUser} /> */}
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
       </div>
     </Router>
   );
