ecce255789f97339e4b5e5c6ef6689e69acc62a8
soulston <simonpoulston@gmail.com>
2020-08-14 12:09:07 +0100

    revert to class component and remove comments

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -72,7 +72,7 @@ export default class CreateExercise extends Component {
       date: this.state.date
     }
 
-    console.log(exercise);
+    // console.log(exercise);
 
     axios.post('http://localhost:5000/exercises/add', exercise)
       .then(res => console.log(res.data));
